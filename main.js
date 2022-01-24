ade4fb223b93abf572a7ad74f7c66e4ba56c68af
kusholhuq <kusholhuq@gmail.com>
2022-01-24 00:21:10 -0800

    getDatasetId

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const url = 'http://api.coxauto-interview.com/api';
+
+
+async function getDatasetId() {
+  try {
+    const responseId = await fetch(`${url}/datasetId`);
+    const dataId = await responseId.json();
+    return dataId;
+  } catch (err) {
+    console.error(err);
+  }
+}
